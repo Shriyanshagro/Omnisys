7cfa50a499338cbb12f6418d51764d5b1b8f64a8
Shriyanshagro <agrawalshriyansh05@gmail.com>
2015-11-14 04:05:52 +0530

    Reorder completely done

--- app/controllers/reorder_controller.rb
+++ app/controllers/reorder_controller.rb
@@ -3,7 +3,8 @@ class ReorderController < ApplicationController
       @report = Report.where('user_id = ?', current_user.id).all
       @name = []
       purchase = []
-      stock = []
+      @risk_stock = []
+      @safe_stock = []
       sold = []
       first = []
       last = []
@@ -12,21 +13,23 @@ class ReorderController < ApplicationController
       @reorder_quantity = []
       @flag = []
       @len = @report.length - 1
+      timer = Time.now.strftime("%Y-%m-%d")
       for i in 0..@len
           @name[i] = @report[i].item_name
           purchase[i] = @report[i].quantity
-          stock[i] = Stock.where("user_id = ? and item_name = ?" , current_user.id , @report[i].item_name).sum(:quantity)
-          sold[i] = purchase[i] - stock[i]
+          @safe_stock[i] = Stock.where("user_id = ? and item_name = ? and expiry_date > ?" , current_user.id , @report[i].item_name, timer).sum(:quantity)
+          @risk_stock[i] = Stock.where("user_id = ? and item_name = ? and expiry_date <= ?" , current_user.id , @report[i].item_name, timer).sum(:quantity)
+          sold[i] = purchase[i] - @safe_stock[i] - @risk_stock[i]
           first[i] = Sale.where("user_id = ? and item_name = ?" , current_user.id , @report[i].item_name).first
           if first[i].present?
               first[i] = first[i].date_of_purchase
               last[i] = Sale.where("user_id = ? and item_name = ?" , current_user.id , @report[i].item_name).last
               last[i] = last[i].date_of_purchase
-              time [i] = (last[i] - first[i]).to_i
+              time[i] = (last[i] - first[i]).to_i
               if time[i] != 0
                   reorder_point[i] = (sold[i]*3)/time[i]
                   @reorder_quantity[i] = reorder_point[i] * 10
-                  if reorder_point[i] >= stock[i]
+                  if reorder_point[i] >= @safe_stock[i]
                       @flag[i] = 2
                   else
                       @flag[i] = 1
